69ad9ee3494788a459baff1a3b5b958ed5060263
gabiroarnaud <gabiroarnaud@gmail.com>
2024-09-03 11:36:25 +0000

    Added video demo component, used next/image for image, used chat area for chatting and changed initial title

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
           <Image
             alt="Authentication Banner"
-            src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            src="./auth-img.avif"
             layout="fill" // Ensures the image fills the parent container
             objectFit="cover" // Ensures the image covers the entire area
             quality={80} // Adjust the quality as needed, default is 75
