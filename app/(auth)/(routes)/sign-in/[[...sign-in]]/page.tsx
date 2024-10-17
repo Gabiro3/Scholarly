1c18c9ac2ca446858f1ba55e078e42e35f4f5857
gabiroarnaud <gabiroarnaud@gmail.com>
2024-10-17 14:00:45 +0000

    Changed auth pages

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,24 +1,7 @@
-import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
+import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return (
-    <section className="bg-white">
-      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-        <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
-          <Image
-            alt="Cover"
-            src="/images/img.avif" // Use the relative path to the image
-            className="absolute inset-0 h-full w-full object-cover"
-          />
-        </aside>
-
-        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-          <SignIn />
-        </main>
-      </div>
-    </section>
-  );
+  return <SignIn />
 }
 
 
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,25 +1,9 @@
-import { SignUp } from "@clerk/nextjs";
-import Image from "next/image";
+import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return (
-    <section className="bg-white">
-      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-        <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
-          <Image
-            alt="Cover"
-            src="/images/img.avif" // Use the relative path to the image
-            className="absolute inset-0 h-full w-full object-cover"
-          />
-        </aside>
-
-        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-          <SignUp />
-        </main>
-      </div>
-    </section>
-  );
+  return <SignUp />
 }
+
 
 
 
